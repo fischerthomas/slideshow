9fb78e391b1c4310530ec2dc12563d85ed3699a2
Thomas Fischer <thomas.fischer@horizn.com>
2015-06-10 13:49:51 -0400

    worked on ie8

--- js/slider-vanilla.js
+++ js/slider-vanilla.js
@@ -1,8 +1,29 @@
 /*
 
     SlideShow: 
-    
+    Html =>
+
+    <div id="" class="slideshow" data-options="">
+      <!-- slides -->
+      <div class="slideshow-wrapper">        
+          <?php for($i = 0; $i < count($pdf_data); $i++) {
+                if(trim($pdf_data[$i])) echo "<figure class='preview-image'><img alt='' width='100%' src='".$pdf_data[$i]."'></figure>";
+          } ?>
+      </div>
+      <!-- nav -->
+      <nav><ul></ul></nav>
+
+    </div>
+
+
+    data options: add autoplay and a speed in ms
 */
+
+// classList Polyfill
+
+/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
+;if("document" in self&&!("classList" in document.createElement("_"))){(function(j){"use strict";if(!("Element" in j)){return}var a="classList",f="prototype",m=j.Element[f],b=Object,k=String[f].trim||function(){return this.replace(/^\s+|\s+$/g,"")},c=Array[f].indexOf||function(q){var p=0,o=this.length;for(;p<o;p++){if(p in this&&this[p]===q){return p}}return -1},n=function(o,p){this.name=o;this.code=DOMException[o];this.message=p},g=function(p,o){if(o===""){throw new n("SYNTAX_ERR","An invalid or illegal string was specified")}if(/\s/.test(o)){throw new n("INVALID_CHARACTER_ERR","String contains an invalid character")}return c.call(p,o)},d=function(s){var r=k.call(s.getAttribute("class")||""),q=r?r.split(/\s+/):[],p=0,o=q.length;for(;p<o;p++){this.push(q[p])}this._updateClassName=function(){s.setAttribute("class",this.toString())}},e=d[f]=[],i=function(){return new d(this)};n[f]=Error[f];e.item=function(o){return this[o]||null};e.contains=function(o){o+="";return g(this,o)!==-1};e.add=function(){var s=arguments,r=0,p=s.length,q,o=false;do{q=s[r]+"";if(g(this,q)===-1){this.push(q);o=true}}while(++r<p);if(o){this._updateClassName()}};e.remove=function(){var t=arguments,s=0,p=t.length,r,o=false;do{r=t[s]+"";var q=g(this,r);if(q!==-1){this.splice(q,1);o=true}}while(++s<p);if(o){this._updateClassName()}};e.toggle=function(p,q){p+="";var o=this.contains(p),r=o?q!==true&&"remove":q!==false&&"add";if(r){this[r](p)}return !o};e.toString=function(){return this.join(" ")};if(b.defineProperty){var l={get:i,enumerable:true,configurable:true};try{b.defineProperty(m,a,l)}catch(h){if(h.number===-2146823252){l.enumerable=false;b.defineProperty(m,a,l)}}}else{if(b[f].__defineGetter__){m.__defineGetter__(a,i)}}}(self))};
+
 
 
 // Instance of the new Object : Slider
@@ -10,7 +31,7 @@ var Slider = function(slideshow){
 
 
     // properties declaration
-    this.slideshow = document.getElementById(slideshow);
+    this.slideshow = slideshow ? document.getElementById(slideshow) : document.getElementsByClassName('slideshow')[0];
     this.nav = this.slideshow.getElementsByTagName('nav')[0];
     this.previews = this.slideshow.getElementsByTagName('figure');
     this.currentPreview = 0;
@@ -34,16 +55,17 @@ Slider.prototype.initSlider = function(){
         this.previews[0].classList.add("show");
 
         var that = this;
+        var i,
+            previewsLength = this.previews.length;
 
-
-        // Iterate the previews
-        Array.prototype.forEach.call(this.previews, function (preview, i) {
+        for(i = 0; i< previewsLength; i++){
 
             // Create footer circular icon
             var li = document.createElement("li");
             li.innerHTML = '<a href="#"></a>';
 
-            that.nav.firstChild.appendChild(li);
+
+            that.nav.getElementsByTagName("ul")[0].appendChild(li);
             that.bullets = that.nav.getElementsByTagName('a');
 
             //Show the first image
@@ -51,21 +73,27 @@ Slider.prototype.initSlider = function(){
 
             var liLink = li.firstChild;
 
+            // closure
+            (function(index){
 
-            //Bind click event
-            liLink.addEventListener("click", function (e) {
+                //Bind click event
+                liLink.addEventListener("click", function (e) {
 
-                e.preventDefault();
-                
-                // if the slideshow is autPlaying stop it
-                if(that.autoplayTimer){
-                    window.clearInterval(that.autoplayTimer);
-                }
-                //show selected image
-                that.showSelectedSlide(i, that.bullets);
+                    e.preventDefault();
+                    
+                    // if the slideshow is autPlaying stop it
+                    if(that.autoplayTimer){
+                        window.clearInterval(that.autoplayTimer);
+                    }
+                    //show selected image
+                    that.showSelectedSlide(index, that.bullets);
 
-            });
-        });
+                });
+       
+
+            })(i)
+
+        }
 
     }
 
@@ -117,34 +145,34 @@ Slider.prototype.autoplay =  function(speed){
 Slider.prototype.showSelectedSlide =  function(index, bullets){
 
     //this.bullets = bullets;
+    var i,
+        previewsLength = this.previews.length;
 
-    Array.prototype.forEach.call(this.previews, function (preview, i) {
+    for(i = 0; i< previewsLength; i++){
         
         if(i === index) {
-            preview.classList.add("show");
+            this.previews[i].classList.add("show");
         } else {
-            preview.classList.remove("show");
+            this.previews[i].classList.remove("show");
         }
-    });
+    };
 
 
-    Array.prototype.forEach.call(this.bullets, function (bullet, i) {
+    for(i = 0; i< previewsLength; i++){
 
         if(i === index) {
-            bullet.classList.add("active");
+            bullets[i].classList.add("active");
         } else {
-            bullet.classList.remove("active");
+            bullets[i].classList.remove("active");
         }
-    });
+    };
 
 };
-
 
 
 // If there is more than 1 slideshow in the same page, give them different id
 var slider1 = new Slider('slideshow-1');
 var slider2 = new Slider('slideshow-2');
-
 
 
 
