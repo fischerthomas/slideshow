80aa06485ee78c4d526dbe33044e6ec0e212a4d2
Thomas Fischer <thomas.fischer@horizn.com>
2015-06-24 10:48:59 -0400

    add main js file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+    Main
+    
+*/
+
+
+(function($){
+
+    // If there is more than 1 slideshow in the same page, give them different id
+    var slider1 = new Slider('slideshow-1');
+    var slider2 = new Slider('slideshow-2');
+
+
+})(jQuery);
--- js/slider-horizn.js
+++ js/slider-horizn.js
@@ -137,12 +137,3 @@ Slider.prototype.showSelectedSlide =  function(index, bullets){
 
 };
 
-
-(function($){
-
-    // If there is more than 1 slideshow in the same page, give them different id
-    var slider1 = new Slider('slideshow-1');
-    var slider2 = new Slider('slideshow-2');
-
-
-})(jQuery);
